6f62a3bccf343bc378e68b091ce76518e9b477f1
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-08-10 11:15:57 -0400

    Prove search server query pipeline

--- SmolSearch.Server/Program.cs
+++ SmolSearch.Server/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SmolSearch.Storage;
+
+var database = new SmolSearchDatabase("smolsearch.db");
+await database.InitializeAsync();
+
+var documents = database.CreateDocumentStore();
+
+Console.Write("Search: ");
+
+var query = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(query))
+{
+    return;
+}
+
+var results = await documents.SearchAsync(query, 10);
+
+Console.WriteLine();
+Console.WriteLine($"# Search results for: {query}");
+Console.WriteLine();
+
+foreach (var result in results)
+{
+    Console.WriteLine($"=> {result.Url} {result.Title ?? result.Url}");
+}
